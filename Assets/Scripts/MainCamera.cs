2686637329928756ba9f864dc23b9d03daed753b
cskilbeck <github@charlie.skilbeck.com>
2014-10-15 22:24:10 +0100

    ..

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,43 +1,44 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿    using UnityEngine;
+    using System.Collections;
 
-public class MainCamera : MonoBehaviour
-{
-    public static Camera cam = CreateCamera();
-    public static LayerMask layer = new LayerMask();
-
-    private static Camera CreateCamera()
+    public class MainCamera : MonoBehaviour
     {
-        GameObject mainCamera = GameObject.FindWithTag("MainCamera");
-        if (mainCamera != null)
+        public static Camera cam = CreateCamera();
+        public static LayerMask layer = new LayerMask();
+
+        private static Camera CreateCamera()
         {
-            GameObject g = GameObject.Instantiate(mainCamera) as GameObject;
-            g.name = "2D Camera";
-            //g.hideFlags = HideFlags.HideAndDontSave;
-            cam = g.camera;
-            cam.hdr = false;
-            cam.renderingPath = RenderingPath.VertexLit;
-            cam.clearFlags = CameraClearFlags.Nothing;
-            cam.orthographic = true;
-            cam.projectionMatrix = Matrix4x4.Ortho(0, Screen.width, 0, Screen.height, 0, 100);
-            cam.DestroyComponent<AudioListener>();
-            cam.DestroyComponent<GUILayer>();
-            foreach (Camera c in GameObject.FindObjectsOfType<Camera>())
+            GameObject mainCamera;
+            mainCamera = GameObject.FindWithTag("MainCamera");
+            if (mainCamera != null)
             {
-                c.cullingMask &= ~(1 << 31);
+                GameObject g = GameObject.Instantiate(mainCamera) as GameObject;
+                g.name = "2D Camera";
+                //g.hideFlags = HideFlags.HideAndDontSave;
+                cam = g.camera;
+                cam.hdr = false;
+                cam.renderingPath = RenderingPath.VertexLit;
+                cam.clearFlags = CameraClearFlags.Nothing;
+                cam.orthographic = true;
+                cam.projectionMatrix = Matrix4x4.Ortho(0, Screen.width, 0, Screen.height, 0, 100);
+                cam.DestroyComponent<AudioListener>();
+                cam.DestroyComponent<GUILayer>();
+                foreach (Camera c in GameObject.FindObjectsOfType<Camera>())
+                {
+                    c.cullingMask &= ~(1 << 31);
+                }
+                cam.cullingMask = 1 << 31;
+                cam.enabled = true;
             }
-            cam.cullingMask = 1 << 31;
-            cam.enabled = true;
+            else
+            {
+                Debug.LogError("Need a 'MainCamera' tagged camera to clone from...");
+            }
+            return cam;
         }
-        else
-        {
-            Debug.LogError("Need a 'MainCamera' tagged camera to clone from...");
-        }
-        return cam;
-    }
 
-    public static Camera Get()
-    {
-        return cam;
+        public static Camera Get()
+        {
+            return cam;
+        }
     }
-}
